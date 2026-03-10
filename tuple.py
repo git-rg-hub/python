ee54e4a7790e3554676cf778564ae2604efa3a3d
git-rg-hub <riya16.gharat@gmail.com>
2026-03-10 22:50:03 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -1,24 +1,24 @@
-tup1=(10,20,30,40,50,60)
-print(tup1)
+#Creating a Tuple
+my_tuple = (10, 20, 30, 40, 50)
+print("Original Tuple:", my_tuple)
 
-#print any element print 
-print (tup1 [0],)
+#Accessing Tuple Elements
+print("First element:", my_tuple[0])
+print("Last element:", my_tuple[-2])
+print("Elements from index 1 to 3:", my_tuple[1:4])
 
-#create second tuple
-tup2 = (1,2,3,4,5,6,)
-print(tup2)
+#Nested Tuple
+nested_tuple = (1, 2, (3, 4, 5), 6)
+print("Nested Tuple:", nested_tuple)
+print("Accessing nested element:", nested_tuple[2])
+print("Accessing element inside nested tuple:", nested_tuple[2][1])
 
-#print any element
-print (tup2[-1],)
+#Repetition of Tuple
+repeated_tuple = my_tuple * 2
+print("Repeated Tuple:", repeated_tuple)
 
-#concatenation both tuple
-concatenated_tuple=tup1+tup2
-print(concatenated_tuple)
-
-#repetition of tuple
-repeated_tuple=tup2*3
-print(repeated_tuple)
-
-#nested tuple
-nested_tuple=(1,2,3,(10,20,30,40,50,60),4,5,6)
-print(nested_tuple)
+#Concatenation of Tuples
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+concatenated_tuple = tuple1 + tuple2
+print("Concatenated Tuple:", concatenated_tuple)
